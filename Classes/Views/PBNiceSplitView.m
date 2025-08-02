7996f7304dacffbad189bc0868472e4180f7051d
Josh Bleecher Snyder <josharian@gmail.com>
2025-08-02 14:17:13 -0700

    fix splitview

--- Classes/Views/PBNiceSplitView.m
+++ Classes/Views/PBNiceSplitView.m
@@ -17,24 +17,35 @@ static NSImage *grip;
 {
 	NSString *barPath = [[NSBundle mainBundle] pathForResource:@"mainSplitterBar" ofType:@"tiff"];
 	bar = [[NSImage alloc] initWithContentsOfFile: barPath];
-	[bar setFlipped: YES];
+	if (!bar) {
+		NSLog(@"Failed to load mainSplitterBar.tiff from path: %@", barPath);
+	}
 
 	NSString *gripPath = [[NSBundle mainBundle] pathForResource:@"mainSplitterDimple" ofType:@"tiff"];
 	grip = [[NSImage alloc] initWithContentsOfFile: gripPath];
-	[grip setFlipped: YES];
+	if (!grip) {
+		NSLog(@"Failed to load mainSplitterDimple.tiff from path: %@", gripPath);
+	}
 }
 
 - (void)drawDividerInRect:(NSRect)aRect
 {
+	if (!bar || !grip) {
+		// Fallback to default system drawing if images failed to load
+		[super drawDividerInRect:aRect];
+		return;
+	}
+	
 	// Draw bar and grip onto the canvas
 	NSRect gripRect = aRect;
 	gripRect.origin.x = (NSMidX(aRect) - ([grip size].width/2));
-	gripRect.size.width = 8;
+	gripRect.size.width = [grip size].width;
+	gripRect.size.height = [grip size].height;
+	gripRect.origin.y = (NSMidY(aRect) - ([grip size].height/2));
 	
-	[self lockFocus];
+	// Use modern drawing API
 	[bar drawInRect:aRect fromRect:NSZeroRect operation:NSCompositingOperationCopy fraction:1.0];
 	[grip drawInRect:gripRect fromRect:NSZeroRect operation:NSCompositingOperationSourceOver fraction:1.0];
-	[self unlockFocus];
 }
 
 - (CGFloat)dividerThickness
