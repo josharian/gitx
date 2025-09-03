324cbcd46419dd2cd704139a2aa78fbc5eaebba2
Josh Bleecher Snyder <josharian@gmail.com>
2025-09-02 18:21:12 -0700

    let through short commit messages

--- Classes/Controllers/PBGitCommitController.m
+++ Classes/Controllers/PBGitCommitController.m
@@ -165,7 +165,7 @@
 	}		
 	
 	NSString *commitMessage = [commitMessageView string];
-	if ([commitMessage length] < 3) {
+	if ([commitMessage length] == 0) {
 		[[repository windowController] showMessageSheet:@"Commitmessage missing" infoText:@"Please enter a commit message before committing"];
 		return;
 	}
